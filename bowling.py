bfceab55757c78c5c026e67e8d7dc7cee6af6629
Nyiri András <nyiriandriss@gmail.com>
2017-11-21 12:40:09 +0100

    iterating with enumerate instead of range len

--- bowling.py
+++ bowling.py
@@ -1,21 +1,21 @@
 def score(game):
     """ Calculates the overall score of a bowling game"""
-    
+
     in_first_half = True
     result = Constants.MINIMUM_POINT
     frame = 1
     previous_value = 0
-    for index in range(len(game)):
-        current_value = get_value(game[index])
-        if game[index] == '/':
+    for index, current_element in enumerate(game):
+        current_value = get_value(current_element)
+        if current_element == '/':
             result += Constants.MAXIMUM_POINT - previous_value
         else:
             result += current_value
         if frame < Constants.FRAMES_THRESHOLD and current_value == Constants.MAXIMUM_POINT:
             next_value = get_value(game[index + 1])
-            if game[index] == '/':
+            if current_element == '/':
                 result += next_value
-            elif game[index] == 'X' or game[index] == 'x':
+            elif current_element == 'X' or current_element == 'x':
                 result += next_value
                 if game[index + 2] == '/':
                     result += Constants.MAXIMUM_POINT - next_value
@@ -24,7 +24,7 @@ def score(game):
         if not in_first_half:
             frame += 1
         in_first_half = not in_first_half
-        if game[index].lower() == 'x':
+        if current_element.lower() == 'x':
             in_first_half = True
             frame += 1
         previous_value = current_value
@@ -42,6 +42,7 @@ def get_value(char):
     else:
         raise ValueError()
     return result
+
 
 class Constants():
     FRAMES_THRESHOLD = 10
