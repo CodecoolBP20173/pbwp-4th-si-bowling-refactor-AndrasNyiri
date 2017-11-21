9c62e06f4650c61443782df5532546471731c80f
Nyiri András <nyiriandriss@gmail.com>
2017-11-21 12:29:31 +0100

    Removed magic numbers

--- bowling.py
+++ bowling.py
@@ -1,23 +1,24 @@
 def score(game):
     """ Calculates the overall score of a bowling game"""
-    result = 0
-    frame = 1
+    
     in_first_half = True
+    result = Constants.MINIMUM_POINT
+    frame = 1
     previous_value = 0
     for index in range(len(game)):
         current_value = get_value(game[index])
         if game[index] == '/':
-            result += 10 - previous_value
+            result += Constants.MAXIMUM_POINT - previous_value
         else:
             result += current_value
-        if frame < 10 and current_value == 10:
+        if frame < Constants.FRAMES_THRESHOLD and current_value == Constants.MAXIMUM_POINT:
             next_value = get_value(game[index + 1])
             if game[index] == '/':
                 result += next_value
             elif game[index] == 'X' or game[index] == 'x':
                 result += next_value
                 if game[index + 2] == '/':
-                    result += 10 - next_value
+                    result += Constants.MAXIMUM_POINT - next_value
                 else:
                     result += get_value(game[index + 2])
         if not in_first_half:
@@ -35,9 +36,14 @@ def get_value(char):
     if char >= '1' and char <= '9':
         result = int(char)
     elif char.lower() == 'x' or char == '/':
-        result = 10
+        result = Constants.MAXIMUM_POINT
     elif char == '-':
-        result = 0
+        result = Constants.MINIMUM_POINT
     else:
         raise ValueError()
     return result
+
+class Constants():
+    FRAMES_THRESHOLD = 10
+    MAXIMUM_POINT = 10
+    MINIMUM_POINT = 0
