9fe056a91718e5076d3db3d6577fa3af1393e86d
Nyiri András <nyiriandriss@gmail.com>
2017-11-21 11:56:59 +0100

    Renamed badly named variables, assigned start value to previous_value, simplified more 'if' statments

--- bowling.py
+++ bowling.py
@@ -2,29 +2,28 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
+    previous_value = 0
+    for index in range(len(game)):
+        if game[index] == '/':
+            result += 10 - previous_value
         else:
-            result += get_value(game[i])
-        if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i + 1])
-                if game[i + 2] == '/':
-                    result += 10 - get_value(game[i + 1])
+            result += get_value(game[index])
+        if frame < 10 and get_value(game[index]) == 10:
+            if game[index] == '/':
+                result += get_value(game[index + 1])
+            elif game[index] == 'X' or game[index] == 'x':
+                result += get_value(game[index + 1])
+                if game[index + 2] == '/':
+                    result += 10 - get_value(game[index + 1])
                 else:
-                    result += get_value(game[i + 2])
-        last = get_value(game[i])
+                    result += get_value(game[index + 2])
         if not in_first_half:
             frame += 1
-            in_first_half = True
-        else:
-            in_first_half = False
-        if game[i] == 'X' or game[i] == 'x':
+        in_first_half = not in_first_half
+        if game[index].lower() == 'x':
             in_first_half = True
             frame += 1
+        previous_value = get_value(game[index])
     return result
 
 
