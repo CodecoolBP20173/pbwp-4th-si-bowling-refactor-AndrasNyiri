4242c100096952c9ab3b66d745990f97a5b495a1
Nyiri András <nyiriandriss@gmail.com>
2017-11-21 13:07:51 +0100

    Renamed the variable to return, gave meaning to an indexed value

--- bowling.py
+++ bowling.py
@@ -5,23 +5,24 @@ def score(game):
     """ Calculates the overall score of a bowling game"""
 
     in_first_half = True
-    result = Constants.MINIMUM_POINT
+    overall_score = Constants.MINIMUM_POINT
     frame = 1
     previous_point = 0
     for index, current_character in enumerate(game):
         current_point = get_value(current_character)
         if current_character == '/':
-            result += Constants.MAXIMUM_POINT - previous_point
+            overall_score += Constants.MAXIMUM_POINT - previous_point
         else:
-            result += current_point
+            overall_score += current_point
         if frame < Constants.FRAMES_THRESHOLD and current_point == Constants.MAXIMUM_POINT:
             next_point = get_value(game[index + 1])
-            result += next_point
+            after_next_character = game[index + 2] 
+            overall_score += next_point
             if current_character == 'X' or current_character == 'x':
-                if game[index + 2] == '/':
-                    result += Constants.MAXIMUM_POINT - next_point
+                if after_next_character == '/':
+                    overall_score += Constants.MAXIMUM_POINT - next_point
                 else:
-                    result += get_value(game[index + 2])
+                    overall_score += get_value(after_next_character)
         if not in_first_half:
             frame += 1
         in_first_half = not in_first_half
@@ -29,7 +30,7 @@ def score(game):
             in_first_half = True
             frame += 1
         previous_point = current_point
-    return result
+    return overall_score
 
 
 def get_value(character):
