f4c21084926137b9f4bdcc8792b4873d3ae760b0
Nyiri András <nyiriandriss@gmail.com>
2017-11-24 12:32:37 +0100

    Simplifed more unnecessarily complicated 'if' statements

--- bowling.py
+++ bowling.py
@@ -4,32 +4,28 @@
 def score(game):
     """ Calculates the overall score of a bowling game"""
 
+    game = game.lower()
     in_first_half = True
     overall_score = Constants.MINIMUM_POINT
     frame = 1
-    previous_point = 0
     for index, current_character in enumerate(game):
         current_point = get_value(current_character)
+        overall_score += current_point
         if current_character == '/':
-            overall_score += Constants.MAXIMUM_POINT - previous_point
-        else:
-            overall_score += current_point
+            overall_score -= get_value(game[index - 1])
         if frame < Constants.FRAMES_THRESHOLD and current_point == Constants.MAXIMUM_POINT:
             next_point = get_value(game[index + 1])
-            after_next_character = game[index + 2]
             overall_score += next_point
-            if current_character.lower() == 'x':
-                if after_next_character == '/':
-                    overall_score += Constants.MAXIMUM_POINT - next_point
-                else:
-                    overall_score += get_value(after_next_character)
-        if not in_first_half:
-            frame += 1
-        in_first_half = not in_first_half
-        if current_character.lower() == 'x':
+            if current_character == 'x':
+                overall_score += get_value(game[index + 2])
+                if game[index + 2] == '/':
+                    overall_score -= next_point
+
+        if not in_first_half or current_character == 'x':
             in_first_half = True
             frame += 1
-        previous_point = current_point
+        else:
+            in_first_half = False
     return overall_score
 
 
@@ -39,7 +35,7 @@ def get_value(character):
     point = -1
     if character >= '1' and character <= '9':
         point = int(character)
-    elif character.lower() == 'x' or character == '/':
+    elif character in ['x', '/']:
         point = Constants.MAXIMUM_POINT
     elif character == '-':
         point = Constants.MINIMUM_POINT
